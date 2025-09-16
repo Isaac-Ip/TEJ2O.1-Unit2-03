f33878831e3d87b72c1dab9b980feeca5c3f86b0
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-09-16 13:43:43 -0400

    changed area and perimeter python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -9,16 +9,16 @@ from microbit import *
 
 # Dimensions
 display.scroll("A rectangle has dimensions 5 cm & 3 cm.")
-sleep(0.5)
+sleep(1)
 display.clear()
 
 # Perimeter
 display.scroll("The perimeter would be:" + str(2 * (5 + 3)) + "cm.")
-sleep(0.5)
+sleep(1)
 display.clear()
 
 # Area
 display.scroll("The area would be:" + str(5 * 3) + "cm ^ 2.")
 display.show(Image.HAPPY)
-sleep(0.5)
+sleep()
 display.clear()
